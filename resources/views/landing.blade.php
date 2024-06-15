d70c2610340a1443e558345def8fff52a16d33e9
Rifqi <arifqiza@gmail.com>
2024-06-15 22:06:32 +0700

    landing

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
     <nav class="d-flex justify-content-center bg-dark align-items-center container sticky pb-2 pt-3 text-white">
-        <p>Disini Navbar "Hapus aja kalo mau bikin nav dari scratch"</p>
+        <p>Disini Navbar "Hapus aja line ini kalo mau bikin nav dari scratch"</p>
     </nav>
 
     <div class="d-flex align-items-center container" style="height: 90vh;">
-        <h1>disini hero section "Hapus aja kalo mau bikin nav dari scratch"</h1>
+        <h1>disini hero section "Hapus juga ini kalo mau bikin dari scratch divnya"</h1>
     </div>
 @endsection
