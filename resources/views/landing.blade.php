1536f8b989cc8a1d468f4c72d1e20892397dc420
Rifqi <arifqiza@gmail.com>
2024-06-16 07:33:54 +0700

    Style Internal

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,7 +5,35 @@
         <p>Disini Navbar "Hapus aja line ini kalo mau bikin nav dari scratch"</p>
     </nav>
 
-    <div class="d-flex align-items-center container" style="height: 90vh;">
-        <h1>disini hero section "Hapus juga ini kalo mau bikin dari scratch divnya"</h1>
+    <div class="d-flex align-items-center cgradient rounded-5 p-shadow container mt-5 overflow-hidden pb-4 pt-5">
+        <div class="row px-4">
+            <h3 class="fw-bold text-white">Layanan <span class="rounded-4 c1 c3b px-2 py-1">Kami</span>
+            </h3>
+            <div class="d-flex justify-content-center flex-wrap gap-4 py-4">
+                <div class="col bg-light rounded-4 px-4 py-3">
+                    <i class="bi bi-people-fill fs-1 c1"></i>
+                    <h4 class="c1 fw-bold mb-2 mt-2">Manajemen Informasi Siswa dan Guru</h4>
+                    <p class="fs-5">Mengelola informasi pribadi siswa dan guru dengan mudah dan cepat untuk dipahami</p>
+                </div>
+                <div class="col bg-light rounded-4 px-4 py-3">
+                    <i class="bi bi-door-open-fill fs-1 c1"></i>
+                    <h4 class="c1 fw-bold mb-2 mt-2">Manajemen Informasi Siswa dan Guru</h4>
+                    <p class="fs-5">Kelola data kelas lebih mudah dengan integrasi antar data dalam sistem
+                        dilakukan</p>
+                </div>
+                <div class="col bg-light rounded-4 px-4 py-3">
+                    <i class="bi bi-clipboard2-check-fill fs-1 c1"></i>
+                    <h4 class="c1 fw-bold mb-2 mt-2">Manajemen Informasi Siswa dan Guru</h4>
+                    <p class="fs-5">Mempermudah siswa dalam mengumpulkan tugas dengan alur yang mudah dipahami</p>
+                </div>
+            </div>
+
+        </div>
+        <img src="{{ asset('assets/star.png') }}" class="position-absolute fixed" style="width: 32px; top: 25%; right: 7%"
+            alt="">
+    </div>
+
+    <div class="">
+
     </div>
 @endsection
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -6,9 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Sistem Managemen Tugas</title>
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <!-- Optional: Add your own CSS styles here -->
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..700;1,400..700&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap');
@@ -16,6 +18,33 @@
         body {
             background-color: hsl(0, 0%, 96%);
             font-family: "Nunito", sans-serif;
+        }
+
+        .cgradient {
+            background: rgb(143, 134, 248);
+            background: linear-gradient(125deg, rgba(143, 134, 248, 1) 0%, rgba(91, 79, 225, 1) 100%);
+        }
+
+        .c1 {
+            color: #5B4FE1;
+        }
+
+        .c2 {
+            color: #4FC5EB;
+        }
+
+        .c3 {
+            color: #FCD23A;
+        }
+
+        .c3b {
+            background-color: #FCD23A;
+        }
+
+        .p-shadow {
+            box-shadow: 0px 14px 45px 0px rgba(91, 79, 225, 0.40);
+            -webkit-box-shadow: 0px 14px 45px 0px rgba(91, 79, 225, 0.40);
+            -moz-box-shadow: 0px 14px 45px 0px rgba(91, 79, 225, 0.40);
         }
     </style>
 </head>
